4d3ef612332be921fbcf1848963411dae33a84a7
Fredrik Sandvold <fres@computas.com>
2022-08-15 20:19:28 +0200

    ADD firebase with host, storage, functions etc

---
+++ emoji-toe-app/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnLOvJznrIXrU4xI4EeJcNawnO8NrWPQc",
+  authDomain: "nxt-intro-prosjekt-2022.firebaseapp.com",
+  databaseURL: "https://nxt-intro-prosjekt-2022-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "nxt-intro-prosjekt-2022",
+  storageBucket: "nxt-intro-prosjekt-2022.appspot.com",
+  messagingSenderId: "732154375762",
+  appId: "1:732154375762:web:86b66c70342cde6876b98b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
